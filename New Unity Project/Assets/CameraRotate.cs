3cf53960e82e3bc5e78b288cf3d0d1ebb2d0e978
M-Yankov <cska31ultras@gmail.com>
2017-08-10 23:23:24 +0300

    First unity 3D project demo

---
+++ New Unity Project/Assets/CameraRotate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject rotateAroundtarget;
+    public bool rotateCamera;
+
+    private const float Radius = 80;
+    private float timeCounter = 0;
+    private bool riseUp = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerScript playerScript = this.player.GetComponent<PlayerScript>();
+        playerScript.tourEnded += PlayerScript_tourEnded;
+    }
+
+    private void PlayerScript_tourEnded(object sender, EventArgs e)
+    {
+        Debug.Log("End!");
+        this.riseUp = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+        float x = Mathf.Cos(timeCounter) * Radius;
+        float y = this.transform.position.y;
+        float z = Mathf.Sin(timeCounter) * Radius;
+
+
+        PlayerScript playerScript = this.player.GetComponent<PlayerScript>();
+        if (this.riseUp && this.transform.position.y < 40)
+        {
+            this.transform.Translate(Vector3.up * Time.deltaTime * playerScript.speed);
+            return;
+        }
+        else if (this.transform.position.y >= 40)
+        {
+            //this.transform.position = new Vector3(x, y, z);
+            this.transform.Translate(Vector3.right);
+            this.transform.LookAt(this.rotateAroundtarget.transform);
+        }
+
+        if (playerScript.IsReady && !playerScript.IsTourEnded)
+        {
+            this.transform.LookAt(this.player.transform);
+
+            this.transform.Translate(Vector3.forward * Time.deltaTime * playerScript.speed);
+            this.transform.position = new Vector3(this.transform.position.x, y, this.transform.position.z);
+        }
+        else if (!playerScript.IsReady)
+        {
+            this.transform.LookAt(this.player.transform);
+
+            this.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * (playerScript.speed / 3));
+            this.transform.position = new Vector3(this.transform.position.x, y, this.transform.position.z);
+        }
+
+
+        // works fine - finish this.
+            
+        if (this.rotateCamera && false)
+        {
+            this.transform.position = new Vector3(x, y, z);
+            this.transform.LookAt(Vector3.zero);
+        }
+    }
+}
---
+++ New Unity Project/Assets/PlayerScript.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+
+    public event EventHandler<EventArgs> tourEnded;
+
+    private bool isReady = false;
+    private bool isTourEnded = false;
+    private bool isDemoFinished = false;
+
+    private const int CollisionOffset = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isDemoFinished)
+        {
+            return;
+        }
+
+        if (this.transform.localRotation.eulerAngles.x > 0 && !this.isReady)
+        {
+            this.transform.Rotate(0.5f, 0, 0);
+        }
+        else
+        {
+            this.isReady = true;
+        }
+
+        if (this.isReady && !this.isTourEnded)
+        {
+            // This is the colligiion detection
+            // Ofcourse there is a better way. It will be learned in next lection
+            bool isInXRange = this.transform.position.x - CollisionOffset <= this.target.transform.position.x && this.target.transform.position.x <= this.transform.position.x + CollisionOffset;
+            bool isInZRange = this.transform.position.z - CollisionOffset <= this.target.transform.position.z && this.target.transform.position.z <= this.transform.position.z + CollisionOffset;
+            if (isInXRange && isInZRange)
+            {
+                TargetScript targetScript = this.target.GetComponent<TargetScript>();
+                if (targetScript.nextTarget != null)
+                {
+                    this.target = targetScript.nextTarget;
+                    Debug.Log("Target switched!");
+                }
+                else
+                {
+                    this.isTourEnded = true;
+                }
+            }
+
+            this.transform.LookAt(this.target.transform);
+            this.transform.Translate(Vector3.forward * Time.deltaTime * this.speed);
+        }
+
+        // pattern
+        if ((this.transform.localRotation.eulerAngles.x < 1 ||
+            this.transform.localRotation.eulerAngles.x > 270) &&
+            this.isTourEnded)
+        {
+            this.transform.Rotate(-0.5f, 0, 0);
+        }
+        else if (this.isTourEnded)
+        {
+            this.isDemoFinished = true;
+        }
+
+        if (this.isDemoFinished)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/events/how-to-raise-base-class-events-in-derived-classes
+            EventHandler<EventArgs> handler = this.tourEnded;
+            if (handler != null)
+            {
+                handler(this, new EventArgs());
+            }
+        }
+    }
+
+    public bool IsReady
+    {
+        get
+        {
+            return this.isReady;
+        }
+    }
+
+    public bool IsTourEnded
+    {
+        get
+        {
+            return this.isTourEnded;
+        }
+    }
+}
